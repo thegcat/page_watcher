95931e3344ca35118c8eb1ea6d54f6ab149e2c85
Felix Schäfer <felix@fachschaften.org>
2012-04-16 14:22:18 +0200

    Small improvements

--- page_watcher.rb
+++ page_watcher.rb
@@ -1,4 +1,5 @@
 #!/bin/env ruby
+# encoding: utf-8
 
 require "rubygems"
 require "bundler/setup"
@@ -12,14 +13,14 @@ REPOS_DIR = File.join(BASE_DIR, "gits")
 CONFIG = {
   :ra_vl => {
     :name => "Rechnerarchitektur Vorlesung",
-    :xpath => "//div[@id='page']/table[2]/tbody/tr/td[last()]/a[text()='PDF']/@href",
+    :xpath => "//div[@id='page']/table[2]/tbody/tr/td[last()]/a[text()='PDF']/@href|//div[@id='page']/h2[text()='Materialien']/following-sibling::ul[1]/li/a/@href",
     :url => "http://ls12-www.cs.tu-dortmund.de/daes/de/lehre/lehrveranstaltungen/sommersemester-2012/rechnerarchitektur.html",
     :repo => "u-felix-2012ss-ra",
     :dir => "Vorlesung",
   },
   :ra_ue => {
     :name => "Rechnerarchitektur Übung",
-    :xpath => "//div[@id='page']/table[4]/tbody/tr/td[1]/a[contains(text(),'Blatt')]/@href",
+    :xpath => "//div[@id='page']/table[4]/tbody/tr/td[1 or 3]/a/@href",
     :url => "http://ls12-www.cs.tu-dortmund.de/daes/de/lehre/lehrveranstaltungen/sommersemester-2012/rechnerarchitektur.html",
     :repo => "u-felix-2012ss-ra",
     :dir => "Uebung",
@@ -51,12 +52,10 @@ CONFIG = {
 CONFIG.each do |key, options|
   puts "*** #{options[:name]}"
   links = Nokogiri::HTML(open options[:url]).xpath(options[:xpath]).map(&:to_s)
-  links.each {|link| puts link}
-  puts links.inspect
 
   target_dir = File.join(REPOS_DIR, options[:repo], options[:dir])
   puts %x{cd #{target_dir} && git pull}
-  puts %x{echo '#{links.join("\n")}' | wget -i - -N -P #{target_dir} -B '#{options[:url]}'}
+  puts %x{echo '#{links.join("\n")}' | wget -q -i - -N -P #{target_dir} -B '#{options[:url]}'}
   puts %x{cd #{target_dir} && git add . && git commit -m "Automatically added/updated files" && git push}
   puts
 end
